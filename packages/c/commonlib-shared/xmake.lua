828b5a1f9d04b6690d0659ba846649c1ca80f84b
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-05-18 06:50:50 -0600

    feat: `commonlib-shared` | add `xse_mmio` option

--- packages/c/commonlib-shared/xmake.lua
+++ packages/c/commonlib-shared/xmake.lua
@@ -4,15 +4,13 @@ package("commonlib-shared")
     set_license("GPLv3")
 
     add_urls("https://github.com/libxse/commonlib-shared.git")
-    add_versions("0.1.0", "9a20056954dd6d88af14b83de63d9424791e1a03")
-    add_versions("0.1.1", "b3b9dbcca8a2e81956675d9b9908dfdfad844efd")
 
-    add_configs("rex_ini", { description = "enable ini settings support for REX", default = false, type = "boolean" })
-    add_configs("rex_json", { description = "enable json settings support for REX", default = false, type = "boolean" })
-    add_configs("rex_toml", { description = "enable toml settings support for REX", default = false, type = "boolean" })
+    add_configs("rex_ini", { description = "enable REX::INI settings support", default = false, type = "boolean" })
+    add_configs("rex_json", { description = "enable REX::JSON settings support", default = false, type = "boolean" })
+    add_configs("rex_toml", { description = "enable REX::TOML settings support", default = false, type = "boolean" })
+    add_configs("xse_mmio", { description = "enable mmio based Address Library support", default = false, type = "boolean" })
     add_configs("xse_xbyak", { description = "enable xbyak support for Trampoline", default = false, type = "boolean" })
 
-    add_deps("rsm-mmio")
     add_deps("spdlog", { configs = { header_only = false, wchar = true, std_format = true } })
 
     add_syslinks("advapi32", "bcrypt", "d3d11", "d3dcompiler", "dbghelp", "dxgi", "ole32", "shell32", "user32", "version", "ws2_32")
@@ -30,6 +28,10 @@ package("commonlib-shared")
             package:add("defines", "REX_OPTION_TOML=1")
             package:add("deps", "toml11")
         end
+        if package:config("xse_mmio") then
+            package:add("defines", "XSE_MMIO_ADDRESSLIB=1")
+            package:add("deps", "rsm-mmio")
+        end
         if package:config("xse_xbyak") then
             package:add("defines", "XSE_SUPPORT_XBYAK=1")
             package:add("deps", "xbyak")
@@ -41,6 +43,7 @@ package("commonlib-shared")
             rex_ini = package:config("rex_ini"),
             rex_json = package:config("rex_json"),
             rex_toml = package:config("rex_toml"),
+            xse_mmio = package:config("xse_mmio"),
             xse_xbyak = package:config("xse_xbyak")
         })
     end)
