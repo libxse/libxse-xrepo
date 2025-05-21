47eca6ffcb79208b9f757df369b2ddad322aac16
Qudix <17361645+Qudix@users.noreply.github.com>
2025-05-20 21:02:11 -0500

    feat: `commonlib-shared` | remove `mmio` requirement and unify options

--- packages/c/commonlib-shared/xmake.lua
+++ packages/c/commonlib-shared/xmake.lua
@@ -5,55 +5,45 @@ package("commonlib-shared")
 
     add_urls("https://github.com/libxse/commonlib-shared.git")
 
-    add_configs("rex_ini", { description = "enable REX::INI settings support", default = false, type = "boolean" })
-    add_configs("rex_json", { description = "enable REX::JSON settings support", default = false, type = "boolean" })
-    add_configs("rex_toml", { description = "enable REX::TOML settings support", default = false, type = "boolean" })
-    add_configs("xse_mmio", { description = "enable mmio based Address Library support", default = false, type = "boolean" })
-    add_configs("xse_xbyak", { description = "enable xbyak support for Trampoline", default = false, type = "boolean" })
+    add_configs("ini", { description = "enable REX::INI settings support", default = false, type = "boolean" })
+    add_configs("json", { description = "enable REX::JSON settings support", default = false, type = "boolean" })
+    add_configs("toml", { description = "enable REX::TOML settings support", default = false, type = "boolean" })
+    add_configs("xbyak", { description = "enable xbyak support for Trampoline", default = false, type = "boolean" })
 
     add_deps("spdlog", { configs = { header_only = false, wchar = true, std_format = true } })
 
     add_syslinks("advapi32", "bcrypt", "d3d11", "d3dcompiler", "dbghelp", "dxgi", "ole32", "shell32", "user32", "version", "ws2_32")
 
     on_load("windows|x64", function(package)
-        if package:config("rex_ini") then
-            package:add("defines", "REX_OPTION_INI=1")
+        if package:config("ini") then
+            package:add("defines", "COMMONLIB_OPTION_INI=1")
             package:add("deps", "simpleini")
         end
-        if package:config("rex_json") then
-            package:add("defines", "REX_OPTION_JSON=1")
+        if package:config("json") then
+            package:add("defines", "COMMONLIB_OPTION_JSON=1")
             package:add("deps", "nlohmann_json")
         end
-        if package:config("rex_toml") then
-            package:add("defines", "REX_OPTION_TOML=1")
+        if package:config("toml") then
+            package:add("defines", "COMMONLIB_OPTION_TOML=1")
             package:add("deps", "toml11")
         end
-        if package:config("xse_mmio") then
-            package:add("defines", "XSE_MMIO_ADDRESSLIB=1")
-            package:add("deps", "rsm-mmio")
-        end
-        if package:config("xse_xbyak") then
-            package:add("defines", "XSE_SUPPORT_XBYAK=1")
+        if package:config("xbyak") then
+            package:add("defines", "COMMONLIB_OPTION_XBYAK=1")
             package:add("deps", "xbyak")
         end
     end)
 
     on_install("windows|x64", function(package)
         import("package.tools.xmake").install(package, {
-            rex_ini = package:config("rex_ini"),
-            rex_json = package:config("rex_json"),
-            rex_toml = package:config("rex_toml"),
-            xse_mmio = package:config("xse_mmio"),
-            xse_xbyak = package:config("xse_xbyak")
+            commonlib_ini = package:config("ini"),
+            commonlib_json = package:config("json"),
+            commonlib_toml = package:config("toml"),
+            commonlib_xbyak = package:config("xbyak")
         })
     end)
 
     on_test("windows|x64", function(package)
         assert(package:check_cxxsnippets({test = [[
-            #include <format>
-            #include <source_location>
-            #include <string_view>
-
             #include <REX/REX/LOG.h>
 
             void test() {
